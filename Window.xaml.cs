f61f8174f11d9d108a9996c410b83a4c8b91b957
kenz-gelsoft <kenz-gelsoft@users.noreply.github.com>
2017-05-16 12:11:04 +0900

    イベント処理のサンプル

---
+++ Window.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows;
+
+public partial class NetapadWindow : Window
+{
+    void NetapadWindow_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("クリックされました！");
+    }
+}
