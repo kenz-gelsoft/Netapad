11a52ca2eb3c82c3d41278efc829bc444a284e18
kenz-gelsoft <kenz-gelsoft@users.noreply.github.com>
2017-05-21 09:47:47 +0900

    複数行入力できるテキストボックスとウィンドウを閉じるメニューの構成に変更

--- Window.cs
+++ Window.cs
@@ -7,17 +7,38 @@ public partial class NetapadWindow : Window
     {
         this.Title = "ネタ帳";
 
-        Button button = new Button();
-        button.Content = "Click Me!";
-        button.Width = 100;
-        button.Height = 50;
-        button.Click += NetapadWindow_Click;
+        DockPanel panel = new DockPanel();
 
-        this.Content = button;
+        Menu menuBar = BuildMenuBar();
+        DockPanel.SetDock(menuBar, Dock.Top);
+        panel.Children.Add(menuBar);
+
+        TextBox textBox = new TextBox();
+        textBox.AcceptsReturn = true;
+        panel.Children.Add(textBox);
+
+        this.Content = panel;
     }
 
-    void NetapadWindow_Click(object sender, RoutedEventArgs e)
+    Menu BuildMenuBar()
     {
-        MessageBox.Show("クリックされました！");
+        Menu menuBar = new Menu();
+
+        MenuItem fileMenu = new MenuItem();
+        fileMenu.Header = "ファイル(_F)";
+
+        MenuItem fileExit = new MenuItem();
+        fileExit.Header = "終了(_X)";
+        fileExit.Click += NetapadWindow_FileExitClick;
+        fileMenu.Items.Add(fileExit);
+
+        menuBar.Items.Add(fileMenu);
+
+        return menuBar;
+    }
+
+    void NetapadWindow_FileExitClick(object sender, RoutedEventArgs e)
+    {
+        Close();
     }
 }
