9a17a4c1c06193548aa20aabbe970a0a8e841cc9
kenz-gelsoft <kenz-gelsoft@users.noreply.github.com>
2017-05-24 07:12:57 +0900

    Commandの仕組みを使いつつ編集メニューまでの項目を作成

--- Window.cs
+++ Window.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
-public partial class NetapadWindow : Window
+public class NetapadWindow : Window
 {
     public NetapadWindow()
     {
@@ -9,9 +11,7 @@ public partial class NetapadWindow : Window
 
         DockPanel panel = new DockPanel();
 
-        Menu menuBar = BuildMenuBar();
-        DockPanel.SetDock(menuBar, Dock.Top);
-        panel.Children.Add(menuBar);
+        panel.Children.Add(BuildMenuBar());
 
         TextBox textBox = new TextBox();
         textBox.AcceptsReturn = true;
@@ -20,25 +20,134 @@ public partial class NetapadWindow : Window
         this.Content = panel;
     }
 
+    class MenuDefinition : Tuple<string, MenuItemDefinition[]>
+    {
+        public MenuDefinition(string s, MenuItemDefinition[] items) : base(s, items) {}
+    }
+
+    class MenuItemDefinition : Tuple<string, ICommand>
+    {
+        static MenuItemDefinition separator = new MenuItemDefinition("", null);
+        public static MenuItemDefinition Separator {
+            get {
+                return separator;
+            }
+        }
+        public MenuItemDefinition(string s, ICommand c) : base(s, c) {}
+    }
+
     Menu BuildMenuBar()
     {
         Menu menuBar = new Menu();
+        DockPanel.SetDock(menuBar, Dock.Top);
 
-        MenuItem fileMenu = new MenuItem();
-        fileMenu.Header = "ファイル(_F)";
+        MenuDefinition[] menus = {
+            new MenuDefinition("ファイル(_F)", new MenuItemDefinition[] {
+                new MenuItemDefinition("新規(_N)", ApplicationCommands.New),
+                new MenuItemDefinition("開く(_O)...", ApplicationCommands.Open),
+                new MenuItemDefinition("上書き保存(_S)", ApplicationCommands.Save),
+                new MenuItemDefinition("名前を付けて保存(_A)...", ApplicationCommands.SaveAs),
+                MenuItemDefinition.Separator,
+                new MenuItemDefinition("ページ設定(_U)...", new PageSettingsCommand(this)),
+                new MenuItemDefinition("印刷(_P)...", ApplicationCommands.Print),
+                MenuItemDefinition.Separator,
+                new MenuItemDefinition("ネタ帳の終了(_X)", new ExitCommand(this)),
+            }),
+            new MenuDefinition("編集(_E)", new MenuItemDefinition[] {
+                new MenuItemDefinition("元に戻す(_U)", ApplicationCommands.Undo),
+                MenuItemDefinition.Separator,
+                new MenuItemDefinition("切り取り(_T)", ApplicationCommands.Cut),
+                new MenuItemDefinition("コピー(_C)", ApplicationCommands.Copy),
+                new MenuItemDefinition("貼り付け(_P)", ApplicationCommands.Paste),
+                new MenuItemDefinition("削除(_L)", ApplicationCommands.Delete),
+                MenuItemDefinition.Separator,
+                new MenuItemDefinition("検索(_F)...", ApplicationCommands.Find),
+                new MenuItemDefinition("次を検索(_N)", new FindNextCommand(this)),
+                new MenuItemDefinition("置換(_R)...", ApplicationCommands.Replace),
+                new MenuItemDefinition("行へ移動(_G)...", new GotoCommand(this)),
+                MenuItemDefinition.Separator,
+                new MenuItemDefinition("すべて選択(_A)", ApplicationCommands.SelectAll),
+                new MenuItemDefinition("日付と時刻(_D)...", new DateTimeCommand(this)),
+            }),
+        };
 
-        MenuItem fileExit = new MenuItem();
-        fileExit.Header = "終了(_X)";
-        fileExit.Click += NetapadWindow_FileExitClick;
-        fileMenu.Items.Add(fileExit);
+        foreach (var menuDef in menus)
+        {
+            MenuItem menu = new MenuItem();
+            menu.Header = menuDef.Item1;
 
-        menuBar.Items.Add(fileMenu);
+            foreach (var item in menuDef.Item2)
+            {
+                if (item == MenuItemDefinition.Separator) {
+                    menu.Items.Add(new Separator());
+                    continue;
+                }
+                MenuItem menuItem = new MenuItem();
+                menuItem.Header = item.Item1;
+                menuItem.Command = item.Item2;
+                menu.Items.Add(menuItem);
+            }
 
+            menuBar.Items.Add(menu);
+        }
         return menuBar;
     }
 
-    void NetapadWindow_FileExitClick(object sender, RoutedEventArgs e)
+    class PageSettingsCommand : WindowCommand
     {
-        Close();
+        public PageSettingsCommand(Window w) : base(w) {}
+        public override void Execute(object parameter)
+        {
+            // TODO
+        }
+    }
+    class ExitCommand : WindowCommand
+    {
+        public ExitCommand(Window w) : base(w) {}
+        public override void Execute(object parameter)
+        {
+            window.Close();
+        }
+    }
+    class FindNextCommand : WindowCommand
+    {
+        public FindNextCommand(Window w) : base(w) {}
+        public override void Execute(object parameter)
+        {
+            // TODO
+        }
+    }
+    class GotoCommand : WindowCommand
+    {
+        public GotoCommand(Window w) : base(w) {}
+        public override void Execute(object parameter)
+        {
+            // TODO
+        }
+    }
+    class DateTimeCommand : WindowCommand
+    {
+        public DateTimeCommand(Window w) : base(w) {}
+        public override void Execute(object parameter)
+        {
+            // TODO
+        }
+    }
+
+    abstract class WindowCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged { add {} remove {} }
+
+        protected Window window;
+        public WindowCommand(Window w)
+        {
+            window = w;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public abstract void Execute(object parameter);
     }
 }
