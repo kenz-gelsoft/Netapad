ae5cffa83592cde603221968129d482453590ba6
kenz-gelsoft <kenz-gelsoft@users.noreply.github.com>
2017-06-11 02:46:31 +0900

    Windowsではニーモニックが付くよう調整した

--- wpf/WpfMenuBar.cs
+++ wpf/WpfMenuBar.cs
@@ -17,22 +17,31 @@ namespace Netapad
             foreach (var menuDef in aMenus)
             {
                 var menu = new MenuItem();
-                menu.Header = menuDef.Item1;
+                menu.Header = LabelWithMnemonic(menuDef.Item1, menuDef.Item2);
 
-                foreach (var item in menuDef.Item2)
+                foreach (var item in menuDef.Item3)
                 {
                     if (item == MenuItemDefinition.Separator) {
                         menu.Items.Add(new Separator());
                         continue;
                     }
                     var menuItem = new MenuItem();
-                    menuItem.Header = item.Item1;
-                    menuItem.Command = item.Item2;
+                    menuItem.Header = LabelWithMnemonic(item.Item1, item.Item2);
+                    menuItem.Command = item.Item3;
                     menu.Items.Add(menuItem);
                 }
 
                 menuBar.Items.Add(menu);
             }
+        }
+
+        static string LabelWithMnemonic(string aLabel, string aMnemonic)
+        {
+            var insertPosition = aLabel.LastIndexOf("...");
+            if (insertPosition < 0) {
+                insertPosition = aLabel.Length;
+            }
+            return aLabel.Insert(insertPosition, '(' + aMnemonic + ')');
         }
     }
 }
